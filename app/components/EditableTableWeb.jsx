62fb2ec16740b056aea942b3fa17a6bca3bf2119
BrandonHS <branditis1211@gmail.com>
2025-06-12 12:15:26 -0600

    feat(rfc): Las tablas de rfc ahora actualizan automaticamente el id de cada registro tras borrar

--- app/components/EditableTableWeb.jsx
+++ app/components/EditableTableWeb.jsx
@@ -5,10 +5,7 @@ import EditIcon from "@mui/icons-material/Edit";
 import DeleteIcon from "@mui/icons-material/DeleteOutlined";
 import SaveIcon from "@mui/icons-material/Save";
 import CancelIcon from "@mui/icons-material/Close";
-import Autocomplete from "@mui/material/Autocomplete";
-import TextField from "@mui/material/TextField";
 import movimiento from "../constants/movimiento";
-import ipourl from "../constants/IPoURL";
 import {
   GridRowModes,
   DataGrid,
