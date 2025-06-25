3fada2b319daadbb6cd9dfa1ff29ad6d0772ce61
NidiaVGarcia <nidiavgarcia12@gmail.com>
2025-06-25 17:13:39 -0600

    fix(internet): se agrego que el número es de CONAGUA)

--- app/constants/telefono.jsx
+++ app/constants/telefono.jsx
@@ -1,6 +1,6 @@
 import { FormHelperText } from "@mui/material";
 
 // TELEFONO CONAGUA
-const telefonoAutocomplete = [{ label: "CONAGUA", value: "55-5174-4000" }];
+const telefonoAutocomplete = [{ label: "Número de teléfono de CONAGUA", value: "55-5174-4000" }];
 
 export default telefonoAutocomplete;
