1241d17503d7ac60304ca666e46d45d107d65e13
NidiaVGarcia <nidiavgarcia12@gmail.com>
2025-09-18 08:56:44 -0600

    fix(version):versión de desarrollo

--- app/layout.jsx
+++ app/layout.jsx
@@ -10,7 +10,7 @@ import "@fontsource/noto-sans";
 
 export const metadata = {
   description: "Formularios de solicitudes GTIC",
-  version: "1.0.0",
+  version: "1.0.8",
   author: "SSTTS (Brandon y Nidia)",
 };
 
